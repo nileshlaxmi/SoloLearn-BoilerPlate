8baf9b9cac38cc71a00d71a5af9ae1f84aacc82c
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2019-01-30 18:27:42 +0530

    add enter key event in single one

--- single/js/app.js
+++ single/js/app.js
@@ -8,6 +8,18 @@ const clickFunction = () => {
     }
 }
 
+const enterKeyFunction = (event) => {
+    if (event.which == 13 || event.keyCode == 13) {
+        let str = document.getElementById('ipText').value;
+        if (str !== "") {
+            document.getElementById('opText').innerHTML = str;
+        }
+        else {
+            document.getElementById('opText').innerHTML = "Please enter a number";
+        }
+    }
+}
+
 function clearFields(){
     document.getElementById('ipText').value = "";
     document.getElementById('opText').innerHTML = "";
